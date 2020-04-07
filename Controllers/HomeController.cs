613cca1f35b59dbe0a8d95f91d922b90cc367e54
felipe rodas <feliper@MacBook-Air-de-felipe.local>
2020-04-07 18:59:41 -0500

    carga inicial test docker compose con mysql

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mysql_nc.Models;
+
+namespace mysql_nc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private LibraryContext _context;
+
+        public HomeController(ILogger<HomeController> logger, LibraryContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        private void InsertData()
+        {
+            
+            // Creates the database if not exists
+            _context.Database.EnsureCreated();
+
+            // Adds a publisher
+            var publisher = new Publisher
+            {
+                Name = "Mariner Books"
+            };
+            
+            _context.Publisher.Add(publisher);
+
+            // Adds some books
+            _context.Book.Add(new Book
+            {
+                ISBN = "978-0544003415",
+                Title = "The Lord of the Rings",
+                Author = "J.R.R. Tolkien",
+                Language = "English",
+                Pages = 1216,
+                Publisher = publisher
+            });
+            
+            _context.Book.Add(new Book
+            {
+                ISBN = "978-0547247762",
+                Title = "The Sealed Letter",
+                Author = "Emma Donoghue",
+                Language = "English",
+                Pages = 416,
+                Publisher = publisher
+            });
+
+            // Saves changes
+            _context.SaveChanges();
+        }
+
+        public IActionResult Index()
+        {
+            
+            //InsertData();
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/LibraryContext.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore.Extensions;
+
+namespace mysql_nc.Models
+{
+  public class LibraryContext : DbContext
+  {
+    public DbSet<Book> Book { get; set; }
+
+    public DbSet<Publisher> Publisher { get; set; }
+  
+    public LibraryContext()
+    {
+    }
+
+    public LibraryContext(DbContextOptions<LibraryContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        //optionsBuilder.UseMySQL("server=localhost;database=library;user=root;password=example");
+      }
+      
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<Publisher>(entity =>
+      {
+        entity.HasKey(e => e.ID);
+        entity.Property(e => e.Name).IsRequired();
+      });
+
+      modelBuilder.Entity<Book>(entity =>
+      {
+        entity.HasKey(e => e.ISBN);
+        entity.Property(e => e.Title).IsRequired();
+        entity.HasOne(d => d.Publisher).WithMany(p => p.Books);
+      });
+    }
+  }
+}
