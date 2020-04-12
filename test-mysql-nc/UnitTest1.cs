ed0c8fc80e7c77c9c3e5e51e1f71dfc4389d14fe
felipe rodas <feliper@MacBook-Air-de-felipe.local>
2020-04-12 13:30:05 -0500

    se agrego proyecto para pruebas unitarias con NUnit

---
+++ test-mysql-nc/UnitTest1.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using MySql.Data.MySqlClient;
+
+namespace test_mysql_nc
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase("server=localhost;database=library;user=root;password=example")]
+        public void TestDbConnection(string stringConnection)
+        {
+            bool retorno = false;
+            MySqlConnection conexion = new MySqlConnection(stringConnection);
+
+            conexion.Open();
+
+            retorno = true;
+
+            conexion.Close();
+
+            Assert.AreEqual(true, retorno);
+        }
+    }
+}
