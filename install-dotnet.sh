26de10bae1e5cb22fc8bd629ef4b637353be5090
felipe rodas <feliper@MacBook-Air-de-felipe.local>
2020-04-12 19:12:03 -0500

    add dotnet sdk install

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -2,6 +2,7 @@ wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get -y install apt-transport-https
+sudo apt-get -y install dotnet-sdk-3.1
 sudo apt-get -y install dotnet-runtime-3.1
 sudo apt-get -y install aspnetcore-runtime-3.1
 dotnet restore test-mysql-nc/test-mysql-nc.csproj
